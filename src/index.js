c33db5843639307cfc2b5212014139ae58e8b20b
PavelAkulovich <pasha860@mail.ru>
2021-08-16 12:17:55 +0300

    feat:finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+//  n = -n;
+//  while(n>0) {
+//      result = result * 10 + n % 10;
+//       n = Math.floor(n / 10);
+ let result = 0;
+ let x = Math.abs(n)
+          while(x) {
+           result = result * 10 + x % 10;
+           x = Math.floor(x / 10);
+         }; 
+ return result;
 }
+      
+  
+   
